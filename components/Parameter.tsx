445a510510e49c4d2521be1b8396d6ea2dd8478a
Harrison Lewis <36082806+Stratiz@users.noreply.github.com>
2025-07-02 17:47:03 -0500

    feat: Default markers & mobile compatible arg list

--- components/Parameter.tsx
+++ components/Parameter.tsx
@@ -9,12 +9,14 @@ const ParameterList = ({children} : {name : string, children : any}) => {
     }
   }
 
-  const modifiedChildren = React.Children.map(children, (child) => {
+  const modifiedChildren = React.Children.map(children, (child, index) => {
+    
+
     return React.cloneElement(child, {sizeOverride : maxSize, sizeCallback});
   })
 
   return <>
-    <div className="flex flex-col w-full mt-6 rounded-md outline-1 outline outline-gb-light-bg3 dark:outline-gb-bg2">
+    <div className="flex flex-col gap-2 w-full mt-6 ">
       {modifiedChildren}
     </div>
   </>
@@ -31,12 +33,12 @@ const Parameter = ({name, type, children, sizeCallback, sizeOverride} : {name : 
   }, [typeContainerRef.current])
 
   return ( 
-    <div className="flex flex-row w-full border-t-[1px] dark:border-gb-bg2 border-gb-light-bg3 ">
-      <div ref={typeContainerRef} style={(sizeOverride ? {minWidth : (sizeOverride + "px")} : {})} className=" min-w-1/3 min-w- dark:bg-gb-bg1 bg-gb-light-bg2 content-center p-2 px-4 border-r-2 border-gb-light-bg3 dark:border-gb-bg2 flex flex-col items-left">
-        <p className='font-semibold text-xl dark:text-gb-faded text-black'>{name}</p>
+    <div className="flex flex-col w-full rounded-md outline-1 outline outline-gb-light-bg3 dark:outline-gb-bg2">
+      <div ref={typeContainerRef} className=" min-w-1/3 min-w- dark:bg-gb-bg1 bg-gb-light-bg2 content-center p-2 px-4 flex flex-row items-center">
+        <p className='font-semibold text-xl dark:text-gb-faded text-black pr-2'>{name}</p>
         <p className="font-semibold texl-xl dark:bg-gb-bg3 bg-gb-light-bg1 bg-opacity-50 h-min w-min p-1 mt-1 text-nowrap rounded-md text-[#444444] dark:text-[#8d8d8d]">{ (type || "any")}</p>
       </div>
-      <div className='w-auto dark:bg-gb-tertiary p-3 content-center text-lg'>
+      <div className='w-auto dark:bg-gb-tertiary p-3 content-center text-md'>
         {children}
       </div>
     </div>
